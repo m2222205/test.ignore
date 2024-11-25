e366808eb668b5ee89fcd377d12a89d2ee10ed35
m2222205 <abduhalilovamaftuna1@gmail.com>
2024-11-25 21:45:50 +0500

    FrontEnd added

--- 9 dars/9 dars/Services/EventService.cs
+++ 9 dars/9 dars/Services/EventService.cs
@@ -9,10 +9,14 @@ namespace _9_dars.Services
         {
             events = new List<Event>();
         }
+        public static void Main(string[] text)
+        {
+            FrontEnd();
 
+        }
         public void FrontEnd()
         {
-            while ()
+            while (true)
             {
                 Console.WriteLine("1. Add event");
                 Console.WriteLine("2. Delete event");
@@ -25,21 +29,140 @@ namespace _9_dars.Services
                 Console.WriteLine("9. Get most Tagged event");
                 Console.WriteLine("Enter you option");
                 var option = int.Parse(Console.ReadLine());
-                if(option == 1)
+
+                else if (option == 1)
                 {
+                    List<Event> events = new List<Event>();
+                    Event event1 = new Event();
                     Console.WriteLine("Title of Event");
-                    var Event.Title = Console.ReadLine();
+                    event1.Title = Console.ReadLine();
+                    Console.WriteLine("Enter Location of Event");
+                    event1.Location = Console.ReadLine();
+                    event1.DateTime = DateTime.Now;
+                    Console.WriteLine("Enter a Description");
+                    event1.Description = Console.ReadLine();
+                    Console.WriteLine("Enter Attandees seperated by space");
+                    string elements = Console.ReadLine();
+                    event1.Attandees.AddRange(elements.Split(' '));
+                    Console.WriteLine("Enter a tags seperated by space");
+                    string el = Console.ReadLine();
+                    event1.Tags.AddRange(el.Split(' '));
+                    AddEvent(event1);
                 }
+                else if (option == 2)
+                {
+                    Console.WriteLine("Enter id for deleting the event");
+                    var id = Guid.Parse((Console.ReadLine()));
+                    var result = DeleteEvent(id);
+                    if (result is true)
+                    {
+                        Console.WriteLine("The Event is Deleted");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: not Deleted");
+                    }
+                }
+                else if (option == 3)
+                {
+                    Console.WriteLine("Enter the ID of old Event");
+                    Guid id = Guid.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter New event");
+                    Event NewEvent = new Event();
+                    Console.WriteLine("Enter a Title of event");
+                    NewEvent.Title = Console.ReadLine();
+                    Console.WriteLine("Title of Event");
+                    NewEvent.Location = Console.ReadLine();
+                    NewEvent.DateTime = DateTime.Now;
+                    Console.WriteLine("Enter a Description");
+                    NewEvent.Description = Console.ReadLine();
+                    Console.WriteLine("Enter Attandees seperated by space");
+                    string elements = Console.ReadLine();
+                    NewEvent.Attandees.AddRange(elements.Split(' '));
+                    Console.WriteLine("Enter a tags seperated by space");
+                    string el = Console.ReadLine();
+                    NewEvent.Tags.AddRange(el.Split(' '));
+                    bool check = UpdateEvent(id, NewEvent);
+                    if (check is true)
+                    {
+                        Console.WriteLine("The was Updated");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error; Not Updated");
+                    }
+                }
+                else if (option == 4)
+                {
 
-
-
-
+                    GetAllEvents();
+                }
+                else if (option == 5)
+                {
+                    Console.WriteLine("Enter the Id");
+                    var Id = Guid.Parse(Console.ReadLine());
+                    var result = GetByid(Id);
+                    if (result is null)
+                    {
+                        Console.WriteLine("Error");
+                    }
+                    else
+                    {
+                        Console.WriteLine(result);
+                    }
+                }
+                else if (option == 6)
+                {
+                    Console.WriteLine("Enter Location");
+                    var Location = Console.ReadLine();
+                    List<Event> SameLocation = GetAllEventsByLocation(Location);
+                    if (SameLocation is null)
+                    {
+                        Console.WriteLine("Error there is Events with such Location");
+                    }
+                    else
+                    {
+                        Console.WriteLine(SameLocation);
+                    }
+                }
+                else if (option == 7)
+                {
+                    List<Event> MostPopular = GetPopularEvent();
+                    Console.WriteLine(MostPopular);
+                }
+                else if (option == 8)
+                {
+                    Console.WriteLine("Enter Person name");
+                    var Name = Console.ReadLine();
+                    Console.WriteLine("Enter events ID");
+                    var ID = Guid.Parse(Console.ReadLine());
+                    var check = AddPersonToEvent(Name, ID);
+                    if (check is true)
+                    {
+                        Console.WriteLine("Person Added");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error not added");
+                    }
+                }
+                else if (option == 9)
+                {
+                    var MostTaged = new List<Event>();
+                    MostTaged = GetMaxTaggedEvent();
+                    if (MostTaged != null)
+                    {
+                        Console.WriteLine(MostTaged);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error there is No most Taged event");
+                    }
+                }
+                Console.ReadKey();
+                Console.Clear();
             }
         }
-
-
-
-
 
         public Event AddEvent(Event newevent)
         {
@@ -98,6 +221,13 @@ namespace _9_dars.Services
                 {
                     SameLocation.Add(i);
 
+                }
+                else
+                {
+                    if (i.Location != location)
+                    {
+                        SameLocation = null;
+                    }
                 }
             }
             return SameLocation;
